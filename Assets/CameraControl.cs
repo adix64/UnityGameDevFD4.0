51b80d76007d9772c9b30c084ec1ba76c542823c
Alex Dinu <adix64@gmail.com>
2020-12-02 17:41:25 +0200

    primu nostru commit

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    float yaw = 0f, pitch = 0f;
+    public Transform player;
+    public float distToTarget = 4f;
+    public float minPitch = -45f, maxPitch = 45f;
+    public Vector3 cameraOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X");
+        pitch -= Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f); //exprimare rotatie camera
+        Vector3 worldSpaceCamOffset = transform.TransformDirection(cameraOffset);
+        transform.position = player.position //de la pozitia personajului
+                            - transform.forward * distToTarget // da camera in spate(ea se uita de-alungul forward)
+                            + worldSpaceCamOffset; // adaugam deplasament pentru incadrare personalizata
+
+    }
+}
---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float playerSpeed = 3f;
+    public float rotSpeed = 10f;
+    public float jumpPower = 5f;
+    public float groundedThreshold = .15f;
+    Transform cameraTransform;
+    Rigidbody rigidbody;
+    CapsuleCollider capsule;
+    Vector3 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        rigidbody = GetComponent<Rigidbody>();
+        capsule = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        GetMoveDirection();
+        ApplyRootMotion();
+        ApplyRootRotation();
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
+        bool grounded = false; //presupun ca nu e pe pamant
+        Ray ray = new Ray();
+        ray.direction = Vector3.down;
+        // 9 raze de deasupra bazei capsulei, aruncate in jos, detecteaza pamantul de sub capsula
+        for (float x = -1f; x <= 1f; x += 1f) 
+        {
+            for (float z = -1f; z <= 1f; z += 1f)
+            {            
+                Vector3 offset = new Vector3(x, 0, z).normalized * capsule.radius;
+                ray.origin = transform.position + offset + Vector3.up * groundedThreshold;
+                if (Physics.Raycast(ray, 2f * groundedThreshold))
+                {
+                    grounded = true;
+                    break;
+                }
+            }
+        }
+        if (grounded)
+        {//sare doar daca e pe pamant
+            if (Input.GetButtonUp("Jump"))
+                rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+        }
+    }
+
+    private void ApplyRootMotion()
+    {
+        //transform.position += dir * playerSpeed * Time.deltaTime;
+        float velY = rigidbody.velocity.y; // retinem viteza pe verticala
+        rigidbody.velocity = moveDir * playerSpeed; //suprascriem viteza cu controlul
+        rigidbody.velocity = new Vector3(rigidbody.velocity.x,
+                                         velY, // pastram componenta verticala calcualta de motorul de fizica
+                                         rigidbody.velocity.z);
+    }
+    private void ApplyRootRotation()
+    {
+        if (moveDir.magnitude < 0.001f)//nu se misca, deci nu roti
+            return;
+        Quaternion newRotation = Quaternion.LookRotation(moveDir); // noua rotatie, ce se uita in directia de miscare
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * rotSpeed);
+    }
+    private void GetMoveDirection()
+    {
+        float h = Input.GetAxis("Horizontal"); //-1 pentru tasta A, 1 pentru tasta D, 0 altfel
+        float v = Input.GetAxis("Vertical"); //-1 pentru tasta S, 1 pentru tasta W, 0 altfel
+        //vectorul directie de miscare, in spatiul lume, relativ la orientarea camerei:
+        moveDir = Vector3.Scale(h * cameraTransform.right +
+                                v * cameraTransform.forward,
+                                new Vector3(1, 0, 1)).normalized;
+    }
+}
